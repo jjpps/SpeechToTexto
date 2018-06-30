4b374a3e0d8daf2397c302b07d6cb94ab4dd7833
jjpps <joaopedro-ps@hotmail.com>
2018-06-30 02:53:12 -0300

    enviado speech from html to node

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express');
-var app = express();
+var app     = express();
+var server  = app.listen(3000);
+var io      = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
   });
 
-
-
-  
-app.listen(3000,function(){
-    console.log('server no :3000');
+io.on('connection',function(socket){
+    socket.on('isFinal',function(data){
+        console.log(data);
+    });
 });
+
+
